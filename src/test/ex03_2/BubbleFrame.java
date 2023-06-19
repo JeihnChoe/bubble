2a895c1718daf1f21a1c75f5ea6a2996828f6fdc
roarroar <baronjae@naver.com>
2023-06-19 17:48:37 +0900

    bubble 최초설정 및 player 이동

---
+++ src/test/ex03_2/BubbleFrame.java
@@ -0,0 +1,65 @@
+package test.ex03_2;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class BubbleFrame extends JFrame {
+
+    private JLabel backgroundMap;
+    private Player player;
+
+    public BubbleFrame() {
+        initObject();
+        initSetting();
+        initListener();
+        setVisible(true);
+
+    }
+
+    private void initObject() {
+        backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+        setContentPane(backgroundMap);
+
+        player = new Player();
+        add(player);
+
+    }
+
+    private void initSetting() {
+        setSize(1000, 640);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    private void initListener() {
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println(e.getKeyCode());
+
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    player.right();
+                }
+
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    player.left();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    player.up();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    player.down();
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new BubbleFrame();
+    }
+}
